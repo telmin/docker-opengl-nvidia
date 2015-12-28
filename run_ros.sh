7bee285a90a0c18b0797fcabe525f88edb62611d
Terumi YAMADA <godo.cell@gmail.com>
2015-12-28 14:47:10 +0900

    add scripts

---
+++ run_ros.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+version_gt() { test "$(echo "$@" | tr " " "\n" | sort -V | tail -n 1)" = "$1"; }
+#docker_version=$(docker version | grep 'Client version' | awk '{split($0,a,":"); print a[2]}' | tr -d ' ')
+docker_version=1.9.0
+# Docker 1.3.0 or later is required for --device
+if ! version_gt "${docker_version}" "1.2.0"; then
+	echo "Docker version 1.3.0 or greater is required"
+	exit 1
+fi
+
+if test $# -lt 1; then
+	# Get the latest opengl-nvidia build
+	# and start with an interactive terminal enabled
+	args="-i -t $(sudo docker images | grep ^opengl-ros | head -n 1 | awk '{ print $1":"$2 }')"
+else
+        # Use this script with derived images, and pass your 'docker run' args
+	args="$@"
+fi
+
+XSOCK=/tmp/.X11-unix
+XAUTH=/tmp/.docker.xauth
+touch $XAUTH
+xauth nlist $DISPLAY | sed -e 's/^..../ffff/' | xauth -f $XAUTH nmerge -
+
+echo $args
+
+sudo docker run \
+	-v $XSOCK:$XSOCK:rw \
+	-v $XAUTH:$XAUTH:rw \
+        --device=/dev/dri/card0:/dev/dri/card0 \
+	-e DISPLAY=$DISPLAY \
+	-e XAUTHORITY=$XAUTH \
+	$args
